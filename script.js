834f3cc15fc7d53072980e947904953b37290ef7
Ethan Martinez <149119742+AdoreEJ-M@users.noreply.github.com>
2024-03-06 19:05:19 -0600

    Change color when mouse hovers

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const container = document.querySelector('.container');
 
 function getGrid() {
-    let num = 16
+    let num = 16;
 
     for (let column = 1; column <= num; column++) {
-        for(let row = 1; row <= num; row++) {
+        for (let row = 1; row <= num; row++) {
             const colors = document.createElement('div');
             colors.setAttribute('class', 'colors');
             container.appendChild(colors);
+
+            function changeColor() {
+                colors.setAttribute('class', 'color-changed');
+            }
+            colors.addEventListener('mouseover', changeColor);
         }
     }
 }
 
 getGrid();
+
